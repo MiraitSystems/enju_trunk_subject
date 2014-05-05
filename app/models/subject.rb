68dd589222454c1ae2d9d5847432ca1443fb20f8
Akifumi NAKAMURA <nakamura.akifumi@miraitsystems.jp>
2014-05-06 00:51:34 +0900

    subject.rbがenju_subjectのv0.1.0.pre5のファイルになっていないので修正。

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,19 +1,27 @@
 class Subject < ActiveRecord::Base
   attr_accessible :parent_id, :use_term_id, :term, :term_transcription,
-    :subject_type_id, :note, :required_role_id, :subject_heading_type_id
+    :subject_type_id, :note, :required_role_id
 
-  belongs_to :manifestation
+  has_many :work_has_subjects, :dependent => :destroy
+  has_many :works, :through => :work_has_subjects, :class_name => 'Manifestation'
   belongs_to :subject_type
-  belongs_to :subject_heading_type
+  has_many :subject_has_classifications, :dependent => :destroy
+  has_many :classifications, :through => :subject_has_classifications
+  belongs_to :subject_type, :validate => true
+  has_many :subject_heading_type_has_subjects
+  has_many :subject_heading_types, :through => :subject_heading_type_has_subjects
   belongs_to :required_role, :class_name => 'Role', :foreign_key => 'required_role_id'
 
-  validates_associated :subject_type, :subject_heading_type
-  validates_presence_of :term, :subject_type_id, :subject_heading_type_id
+  validates_associated :subject_type
+  validates_presence_of :term, :subject_type
+
+  attr_accessor :classification_id, :subject_heading_type_id
 
   searchable do
     text :term
     time :created_at
     integer :required_role_id
+    integer :work_ids, :multiple => true
   end
 
   normalize_attributes :term
@@ -25,21 +33,18 @@ end
 #
 # Table name: subjects
 #
-#  id                      :integer          not null, primary key
-#  parent_id               :integer
-#  use_term_id             :integer
-#  term                    :string(255)
-#  term_transcription      :text
-#  subject_type_id         :integer          not null
-#  scope_note              :text
-#  note                    :text
-#  required_role_id        :integer          default(1), not null
-#  lock_version            :integer          default(0), not null
-#  created_at              :datetime
-#  updated_at              :datetime
-#  deleted_at              :datetime
-#  url                     :string(255)
-#  manifestation_id        :integer
-#  subject_heading_type_id :integer
+#  id                 :integer          not null, primary key
+#  parent_id          :integer
+#  use_term_id        :integer
+#  term               :string(255)
+#  term_transcription :text
+#  subject_type_id    :integer          not null
+#  scope_note         :text
+#  note               :text
+#  required_role_id   :integer          default(1), not null
+#  lock_version       :integer          default(0), not null
+#  created_at         :datetime
+#  updated_at         :datetime
+#  deleted_at         :datetime
 #
 
--- lib/enju_subject/version.rb
+++ lib/enju_subject/version.rb
@@ -1,3 +1,3 @@
 module EnjuTrunkSubject
-  VERSION = "1.0.2"
+  VERSION = "1.0.3"
 end
