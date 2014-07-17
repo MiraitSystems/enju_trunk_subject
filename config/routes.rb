57367165d20e3721b57cbe27734ea3093c18bd5e
Akifumi NAKAMURA <nakamura.akifumi@miraitsystems.jp>
2014-07-17 16:54:58 +0900

    refs 7770 enju_trunk_subjectにあるべきルート情報を移動。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,28 @@
 Rails.application.routes.draw do
-  resources :subject_heading_types
-  resources :classification_types
-  resources :classifications
+  resources :subject_has_classifications
+  resources :subject_heading_types do
+    resources :subjects
+  end
   resources :subject_types
-  resources :subjects
+  resources :subjects do
+    resources :works, :controller => 'manifestations'
+    resources :subject_heading_types
+    resources :subject_has_classifications
+    resources :work_has_subjects
+    resources :classifications
+  end
+  resources :subject_heading_type_has_subjects
+
+  match 'subjects/search_name' => 'subjects#search_name'
+
+  match 'classifications/search_name' => 'classifications#search_name'
+  resources :classifications do
+    resources :subject_has_classifications
+  end
+  resources :classification_types do
+    resource :classifications
+  end
+
+  resources :work_has_subjects
+
 end
